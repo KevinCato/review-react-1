c06004e1c6dce0510779051e6d2214517afe753b
Kevin Cajahuanca <kncajahuancat@unac.edu.pe>
2023-10-25 18:13:23 -0500

    update

---
+++ src/assets/global.jsx
@@ -0,0 +1,9 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+export const global = css`
+  body {
+    font-size: 1rem;
+    line-height: 1.5;
+  }
+`;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Global } from "@emotion/react";
+import { reset } from "./assets/reset.jsx";
+import { global } from "./assets/global.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Global styles={global} />
+    <Global styles={reset} />
     <App />
   </React.StrictMode>
 );
