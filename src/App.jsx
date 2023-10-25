f9ba3658665d8a986f55d6b8aed799ebd7af9f1c
Kevin Cajahuanca <kncajahuancat@unac.edu.pe>
2023-10-25 15:43:44 -0500

    initial component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import Content from "./Components/Content/Content";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+
 function App() {
-  return <div>Init</div>;
+  return (
+    <div>
+      <p>Home</p>
+      <Header></Header>
+      <Content></Content>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default App;
